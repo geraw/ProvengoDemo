bdc53891b1cc1654620d05cd439d63ffa59286e6
gera <gera.weiss@gmail.com>
2025-03-15 11:06:42 +0200

    Added text

--- reset.py
+++ reset.py
@@ -3,5 +3,5 @@ import requests
 test_data = {
     'users': [{ 'id': 1, 'name': 'Test User' }],
     'books': {'1': { 'id': '1', 'title': 'Test Book' }}}
-requests.post('http://192.168.56.1:30889/reset', json=test_data)
+requests.post('http://192.168.56.1:50903/reset', json=test_data)
     

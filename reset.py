ace709cca38f2670266232c0527e7612a312930f
gera <gera.weiss@gmail.com>
2025-03-15 12:18:08 +0200

    Update .gitignore to include reset.py and modify reset.py for API URL change

--- reset.py
+++ reset.py
@@ -1,7 +1,6 @@
-
 import requests
 test_data = {
     'users': [{ 'id': 1, 'name': 'Test User' }],
-    'books': {'1': { 'id': '1', 'title': 'Test Book' }}}
-requests.post('http://192.168.56.1:50903/reset', json=test_data)
-    
+    'books': {'1': { 'id': '1', 'title': 'Test Book' }}
+}
+requests.post('http://your-api-url.com/reset', json=test_data)

da23a0d27e86fcee29371e1b29102fe431070881
gera <gera.weiss@gmail.com>
2025-03-15 21:21:59 +0200

    Remove before_script.sh as it is no longer needed

--- before_script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST 'http://192.168.56.1:53105/reset' -H 'Content-Type: application/json' -d '{"users": [{"id": 1, "name": "Test User"}], "books": {"1": {"id": "1", "title": "Test Book"}}}'

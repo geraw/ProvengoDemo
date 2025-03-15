948c56d030886cc0741be4ee20b4409f5c53285c
gera <gera.weiss@gmail.com>
2025-03-15 21:22:47 +0200

    Remove reset.py and update .gitignore to reflect changes

--- reset.py
+++
@@ -1,6 +0,0 @@
-import requests
-test_data = {
-    'users': [{ 'id': 1, 'name': 'Test User' }],
-    'books': {'1': { 'id': '1', 'title': 'Test Book' }}
-}
-requests.post('http://192.168.56.1:25221/reset', json=test_data)
